de286513ff3eebe392874c316ed72a65b46edb2c
NguyenDuyPhong1204 <nguyenduyphong12122004@gmail.com>
2024-01-23 13:30:32 +0700

    Animal

--- src/oop_quanlysothu/Animal.java
+++ src/oop_quanlysothu/Animal.java
@@ -9,5 +9,78 @@ package oop_quanlysothu;
  * @author Admin
  */
 public class Animal {
+        private String tenDongVat;
+        private String gioiTinh;
+        private int kichThuoc;
+        private int canNang;
+        private String mauSac;
+        private String thucAn;
+        private int tuoi;
+
+    public Animal() {
+    }
+
+    public String getTenDongVat() {
+        return tenDongVat;
+    }
+
+    public void setTenDongVat(String tenDongVat) {
+        this.tenDongVat = tenDongVat;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public int getKichThuoc() {
+        return kichThuoc;
+    }
+
+    public void setKichThuoc(int kichThuoc) {
+        this.kichThuoc = kichThuoc;
+    }
+
+    public int getCanNang() {
+        return canNang;
+    }
+
+    public void setCanNang(int canNang) {
+        this.canNang = canNang;
+    }
+
+    public String getMauSac() {
+        return mauSac;
+    }
+
+    public void setMauSac(String mauSac) {
+        this.mauSac = mauSac;
+    }
+
+    public String getThucAn() {
+        return thucAn;
+    }
+
+    public void setThucAn(String thucAn) {
+        this.thucAn = thucAn;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
     
+    public void nhap(){
+        
+    };
+    
+    public void xuat(){
+        
+    };
 }
